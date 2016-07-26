4885301b5ae23b9ace5f3dc3dd8320f87eb94f21
Melvin Juárez <malvinjazz@live.com>
2016-07-26 17:17:14 -0600

    Intento fallido: no se pudo agregar menu, estilo de cabecera agregado.

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,10 +16,40 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+// var myScroll, myScrollMenu, cuerpo, wrapper, menuprincipal, estado;
+// var xhReq = new XMLHttpRequest();
+//
+// cuerpo = document.getElementById("content");
+// menuprincipal = document.getElementById("menuprincipal");
+// wrapper = document.getElementById("wrapper");
+
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+      // estado="cuerpo";
+      // var heightCuerpo=window.innerHeight-46;
+      // var style = document.createElement('style');
+      // style.type = 'text/css';
+      // style.innerHTML = '.cssClass { position:absolute; z-index:2; left:0; top:46px; width:100%; height: '+heightCuerpo+'px; overflow:auto;}';
+      // document.getElementsByTagName('head')[0].appendChild(style);
+      //
+      // cuerpo.className = 'page center';
+      // menuprincipal.className = 'page center';
+      // wrapper.className = 'cssClass';
+      //
+      // xhReq.open("GET", "opciones/denuncia.html", false);
+  		// xhReq.send(null);
+  		// document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
+      //
+      // // Leemos por ajax el archivos menu.html de la carpeta opciones
+      // xhReq.open("GET", "opciones/menu.html", false);
+      // xhReq.send(null);
+      // document.getElementById("contenidoMenu").innerHTML=xhReq.responseText;
+      //
+      // myScrollMenu = new IScroll('#wrapperMenu', { hideScrollbar: true });
+
+      this.bindEvents();
     },
     // Bind Event Listeners
     //
@@ -330,5 +360,64 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// Funci�n para a�adir clases css a elementos
+// function addClass( classname, element ) {
+//     var cn = element.className;
+//     if( cn.indexOf( classname ) != -1 ) {
+//     	return;
+//     }
+//     if( cn != '' ) {
+//     	classname = ' '+classname;
+//     }
+//     element.className = cn+classname;
+// }
+//
+// // Funci�n para eliminar clases css a elementos
+// function removeClass( classname, element ) {
+//     var cn = element.className;
+//     var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
+//     cn = cn.replace( rxp, '' );
+//     element.className = cn;
+// }
+//
+// function menu(opcion){
+//
+// 	// Si pulsamos en el bot�n de "menu" entramos en el if
+// 	if(opcion=="menu"){
+// 		if(estado=="cuerpo"){
+// 			cuerpo.className = 'page transition right';
+// 			estado="menuprincipal";
+// 		}else if(estado=="menuprincipal"){
+// 			cuerpo.className = 'page transition center';
+// 			estado="cuerpo";
+// 		}
+// 	// Si pulsamos un bot�n del menu principal entramos en el else
+// 	}else{
+//
+// 		// A�adimos la clase al li presionado
+// 		addClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
+//
+// 		// // Recogemos mediante ajax el contenido del html seg�n la opci�n clickeada en el menu
+// 		// xhReq.open("GET", "opciones/opcion"+opcion+".html", false);
+// 		// xhReq.send(null);
+// 		// document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
+//
+// 		// Refrescamos el elemento iscroll seg�n el contenido ya a�adido mediante ajax, y hacemos que se desplace al top
+// 		myScroll.refresh();
+// 		myScroll.scrollTo(0,0);
+//
+// 		// A�adimos las clases necesarias para que la capa cuerpo se mueva al centro de nuestra app y muestre el contenido
+// 		cuerpo.className = 'page transition center';
+// 		estado="cuerpo";
+//
+// 		// Quitamos la clase a�adida al li que hemos presionado
+// 		setTimeout(function() {
+// 			removeClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
+// 		}, 300);
+//
+// 	 }
+//
+// }
 
 app.initialize();
