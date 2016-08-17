3a6f7d202f95a1ea0e117c35fc6748b69cf452f4
Melvin Juárez <malvinjazz@live.com>
2016-08-16 18:23:14 -0600

    Conexion verificada

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -661,7 +661,18 @@ function menu(opcion){
 
 
     if(opcion == '2'){
-      google.charts.setOnLoadCallback(drawGeoChart);
+      try{
+        google.charts.setOnLoadCallback(drawGeoChart);
+      }catch(err){
+        navigator.notification.alert(
+          // 'Necesitas estar conectado para usar este apartado.',
+          err,
+            null,
+          'Denuncia Movil',
+          'Continuar'
+        );
+        return;
+      }
       // var graficas = new iScroll('graficas', {
     	// snap: 'li',
     	// momentum: false,
@@ -1033,15 +1044,16 @@ function sleep(miliseconds) {
 function checkConnection() {
     var networkState = navigator.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+    return networkState;
+    // var states = {};
+    // states[Connection.UNKNOWN]  = 'Unknown connection';
+    // states[Connection.ETHERNET] = 'Ethernet connection';
+    // states[Connection.WIFI]     = 'WiFi connection';
+    // states[Connection.CELL_2G]  = 'Cell 2G connection';
+    // states[Connection.CELL_3G]  = 'Cell 3G connection';
+    // states[Connection.CELL_4G]  = 'Cell 4G connection';
+    // states[Connection.CELL]     = 'Cell generic connection';
+    // states[Connection.NONE]     = 'No network connection';
 
-    alert('Connection type: ' + states[networkState]);
+    // alert('Connection type: ' + states[networkState]);
 }
